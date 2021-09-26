979519c21b22ec2479375e729a97c414ec057c6f
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-09-26 17:55:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="cs/style.css">
+    <title>Vu's Cake</title>
+  </head>
+  <body>
+    <!-- Start code head -->
+     <div class="container-fluid">
+        <div class = 'head'>
+        <div class="row justify-content-start">
+      <div class="col-6 ">
+        <img src="img/logo.png" alt="">
+      </div>
+      <div class="col-6 ">
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Conect</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+        </div> <!-- End code head at here -->
+            <!-- Start code Search -->
+            <div class ='search' style=' background-image: url(img/bg.jpg);height: 250px;width: 100%;'>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search for Foods...." aria-label="Search" style ='height: 40px; width: 500px;'>
+            <button class="btn btn-outline-danger" type="submit">Search</button>
+            </form>
+            </div><!-- End code search at here -->
+
+            <!-- Start Code body-explore -->
+            <div class="body-explore">
+      <h1>Explore Foods</h1>
+      <div class="row">
+        <div class="col-md-3 ms-md-auto"
+          style="background: url(img/menu-burger.jpg)no-repeat; background-size: cover; ">
+          <p>burger</p>
+        </div>
+        <div class="col-md-3 ms-md-auto"
+          style="background: url(img/menu-momo.jpg)no-repeat; background-size: cover; ">
+          <p>momo</p>
+        </div>
+        <div class="col-md-3 ms-md-auto"
+          style="background: url(img/menu-pizza.jpg)no-repeat; background-size: cover; ">
+          <p>pizza</p>
+        </div>
+      </div>
+    </div>
+<!-- End code body-explore at here-->
+
+            <div class ='body-menu' style ='background-color:silver;height:500px;'>
+                <div style ='padding-top:25px'><h2 style='text-align:center; margin:30px;'>Food Menu</h2></div>
+            <div class="container">
+  <div class="row g-2">
+    <div class="col-6">
+      <div class="p-3 border bg-light">
+        <img src='img/menu-momo.jpg' style='height:100px; float:left;margin: 10px 15px 10px 10px'></img>
+        
+        <h5><Strong>Dumplings Specials</Strong></h5>
+        <span><Strong>$5.00</Strong></span>
+        <p>Chicken Dumpling with herbs from Moutains</p>
+        <button style='background:rgb(189, 93, 93); color:white'>Order Now</button>
+        
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light"><img src='img/menu-burger.jpg' style='height:100px; float:left;margin: 10px 15px 10px 10px'></img>
+        
+        <h5><Strong>Best burger</Strong></h5>
+        <span><Strong>$4.00</Strong></span>
+        <p>Burger with Ham, Pineapple and lots of Cheese</p>
+        <button style='background:rgb(189, 93, 93); color:white'>Order Now</button>
+        </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light"><img src='img/menu-momo.jpg' style='height:100px; float:left;margin: 10px 15px 10px 10px'></img>
+        
+        <h5><Strong>Sadeko Momo</Strong></h5>
+        <span><Strong>$6.00</Strong></span>
+        <p>Best Spicy Momo for Winter</p>
+        <button style='background:rgb(189, 93, 93); color:white'>Order Now</button>
+        </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light"><img src='img/menu-pizza.jpg' style='height:110px; float:left;margin: 10px 15px 10px 10px'></img>
+        
+        <h5><Strong>Mixed Pizza</Strong></h5>
+        <span><Strong>$10.00</Strong></span>
+        <p>Pizza with chicken, Ham, Buff, Mushroom </p>
+        <button style='background:rgb(189, 93, 93); color:white'>Order Now</button>
+        </div>
+       </div>
+    </div>
+    
+      
+      </div>
+        <div style ='padding-top: 30px'><a style='color:red;padding-left:46%;'>See all foods</a></div>
+            </div>
+            <!-- End code body-menu at here -->
+
+            <div class ='logo-f'style ='text-align: center; margin: 20px 0'>
+              <img src='img/fb.png' alt='' style='height:40px'></img>
+              <img src='img/ins.png' alt='' style='height:40px'></img>
+              <img src='img/twi.png' alt='' style='height:40px'></img>
+              
+            </div>
+            <!-- end code logo fb at here -->
+            <div class = 'copy'>
+              <h6 style ='text-align: center'>All rights reserved. Designed By <a href='#' style='color:pink; text-decoration:none;'>Nguyễn Minh Vũ</a></h6>
+            </div>
+
+            
+            
+        
+    </div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
